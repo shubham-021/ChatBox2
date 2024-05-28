1861de2e59829ede897507f46198591ce9ab0ebd
shubham-021 <shubham.arka@gmail.com>
2024-05-28 18:37:32 +0530

    working app

--- Server/src/index.js
+++ Server/src/index.js
@@ -18,6 +18,7 @@ const io = new Server(server , {
 })
 
 const roomCreators = {}
+const rooms = {}
 
 connectDB()
 .then( () => {
@@ -29,8 +30,12 @@ connectDB()
     io.on('connection',(socket) => {
 
         socket.on('createRoom',(roomCode) => {
-            socket.join(roomCode)
-            roomCreators[roomCode] = socket.id
+            if(!rooms[roomCode]){
+                rooms[roomCode] = new Set();
+            }
+            roomCreators[roomCode] = socket.id;
+            rooms[roomCode].add(socket);
+            socket.join(roomCode);
             console.log(`Room created: ${roomCode} by ${socket.id}`)
             
             const clients = io.sockets.adapter.rooms.get(roomCode);
@@ -38,31 +43,47 @@ connectDB()
         })
 
         socket.on('joinRoom',(roomCode) => {
-            socket.join(roomCode);
-            console.log(`User ${socket.id} joined room: ${roomCode}`);
-            io.to(roomCode).emit('message', 'A new user has joined the room');
-            
+            if (rooms[roomCode]) {
+                rooms[roomCode].add(socket);
+                socket.join(roomCode)
+                console.log(`User ${socket.id} joined room: ${roomCode}`)
+                io.to(roomCode).emit('message' , 'A new user has joined the room')
+            }else{
+                console.log(`Room ${roomCode} does not exist`)
+            }
+
             // Log all clients in the room
             const clients = io.sockets.adapter.rooms.get(roomCode);
             console.log(`Clients in room ${roomCode} after joining:`, clients ? Array.from(clients) : []);
         })
 
         socket.on('chatMessage',({roomCode , message}) => {
-            console.log(`Server received message: ${message} for room: ${roomCode}`);
-            io.to(roomCode).emit('message', message)
-            console.log(`Message sent to room ${roomCode}: ${message}`)
+            if(rooms[roomCode]){
+                console.log(`Server recieved message: ${message} for room: ${roomCode}`)
+                rooms[roomCode].forEach((socket) => {
+                    socket.emit('message', message)
+                });
+                console.log(`Message sent to room ${roomCode} : ${message}`)
+            }else{
+                console.log(`Room ${roomCode} does not exist.`)
+            }
         })
 
         socket.on('leaveRoom',() => {
-            console.log("User disconnected");
-
-            for(const [roomCode , creatorId] of Object.entries(roomCreators)) {
-    
-                if(creatorId === socket.id) {
-                io.to(roomCode).emit('message' , 'The room creator has left. The room will be closed')
-                io.socketsLeave(roomCode)
-                delete roomCreators[roomCode]
-                console.log(`Room destroyed: ${roomCode}`)
+            console.log("User disconnected: " , socket.id);
+            
+            for (const [roomCode , creatorId] of Object.entries(roomCreators)) {
+                if(creatorId === socket.id){
+                    io.to(roomCode).emit('message','The room creator has left. The room is closed.')
+                    Object.entries(rooms).forEach(([roomCode , sockets]) => {
+                        if(sockets.has(socket)){
+                            sockets.delete(socket)
+                            if(sockets.size == 0){
+                                delete rooms[roomCode]
+                                console.log(`Room ${roomCode} has been destroyed`)
+                            }
+                        }
+                    })
                 }
             }
         })
